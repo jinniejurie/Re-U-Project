3da9aeff69b787f3eb532197f5bf467623577596
mynt <patthananmint.v@gmail.com>
2025-05-06 13:45:27 +0700

    merge

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -29,8 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 CORS_ALLOW_ALL_ORIGINS = True
-ALLOWED_HOSTS = []
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
