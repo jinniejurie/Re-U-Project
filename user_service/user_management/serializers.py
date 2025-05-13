4f3b52eac04d00a0f35afd5d674a71e24e855b5a
jinniejurie <rattananname@email.com>
2025-05-13 19:47:15 +0700

    pdate login and register

--- user_service/user_management/serializers.py
+++ user_service/user_management/serializers.py
@@ -24,19 +24,25 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'password2': {'write_only': True}
+        }
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
+        
         try:
             validate_password(attrs['password'])
         except serializers.ValidationError as e:
             raise serializers.ValidationError({"password": list(e.messages)})
+        
         return attrs
 
     def create(self, validated_data):
+        validated_data.pop('password2')  # Remove password2 from the data
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
