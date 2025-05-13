fb1f514b640e98f58ca0134a2f7f99ae988361a7
jinniejurie <rattananname@email.com>
2025-05-13 18:20:08 +0700

    update order system

--- product_service/order_management/admin.py
+++ product_service/order_management/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Order, OrderItem, Cart, CartItem
 
 # Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'status', 'created_at')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'get_product_name', 'product_name', 'price')
+
+    def get_product_name(self, obj):
+        return obj.product.name if obj.product else '-'
+    get_product_name.short_description = 'Product'
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
--- product_service/order_management/models.py
+++ product_service/order_management/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
-
-from django.db import models
-
-# link order to user
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
     email = models.EmailField(max_length=255, default='')
@@ -28,11 +24,12 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id} - {self.first_name} {self.last_name}"
 
-# link order to product
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('product_management.Product', on_delete=models.CASCADE, related_name='order_items')
     product_name = models.CharField(max_length=255, default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.product_name} - {self.price}"
